7e62488dcf764ade9533b4c8c531c011963a65a9
Dimitri Andre <dimitri@ymagyn.com>
2023-04-04 11:18:26 +0200

    add footer

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Header from "./Header";
+import Header from "./Layout/Header";
+import Footer from "./Layout/Footer"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <div style={{ backgroundColor: "black", color: "white", height:  "100vh"}}>
         <Header></Header>
-        <hr style={{margin: 0}}></hr>
+        <Footer></Footer>
     </div>
 );
