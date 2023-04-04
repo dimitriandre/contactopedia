4342e14a6c7ab881cc112f05bb9da416266f122e
Dimitri Andre <dimitri@ymagyn.com>
2023-04-04 16:47:38 +0200

    create contact empty call

--- src/Components/ContactPages/AddContact.jsx
+++ src/Components/ContactPages/AddContact.jsx
@@ -1,36 +1,55 @@
-const AddContact = () => {
-  return (
-    <div>
-      <div className="border col-12 text-white p-2">
-        <div className="row p-2">
-          <div className="col-12 text-white-50">Add a new Contact</div>
-          <div className="col-12 col-md-4 p-1">
-            <input
-              className="form-control form-control-sm"
-              placeholder="Name..."
-            ></input>
-          </div>
-          <div className="col-12 col-md-4 p-1">
-            <input
-              className="form-control form-control-sm"
-              placeholder="Email..."
-            ></input>
-          </div>
-          <div className="col-12 col-md-4 p-1">
-            <input
-              className="form-control form-control-sm"
-              placeholder="Phone..."
-            ></input>
-          </div>
-          <div className="col-12 col-md-4 p-1 offset-md-4">
-            <button className="btn btn-primary btn-sm form-control">
-              Create
-            </button>
-          </div>
+import React from "react";
+
+class AddContact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      errorMessage: undefined,
+      successMessage: undefined,
+    };
+  }
+
+  handleAddContactFormSubmit = (e) => {
+    e.preventDefault();
+    this.props.handleAddContact();
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="border col-12 text-white p-2">
+          <form onSubmit={this.handleAddContactFormSubmit}>
+            <div className="row p-2">
+              <div className="col-12 text-white-50">Add a new Contact</div>
+              <div className="col-12 col-md-4 p-1">
+                <input
+                  className="form-control form-control-sm"
+                  placeholder="Name..."
+                ></input>
+              </div>
+              <div className="col-12 col-md-4 p-1">
+                <input
+                  className="form-control form-control-sm"
+                  placeholder="Email..."
+                ></input>
+              </div>
+              <div className="col-12 col-md-4 p-1">
+                <input
+                  className="form-control form-control-sm"
+                  placeholder="Phone..."
+                ></input>
+              </div>
+              <div className="col-12 col-md-4 p-1 offset-md-4">
+                <button className="btn btn-primary btn-sm form-control">
+                  Create
+                </button>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default AddContact;
