83b82e2165446de9a406cc9f5d11dbeb061406db
Dimitri Andre <dimitri@ymagyn.com>
2023-04-04 11:10:40 +0200

    create contactopedia

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Header from "./components/Header";
-import Counter from "./components/Counter";
+import Header from "./Header";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <div style={{ backgroundColor: "black", color: "white", height:  "100vh"}}>
         <Header></Header>
         <hr style={{margin: 0}}></hr>
-        <Counter></Counter>
     </div>
 );
